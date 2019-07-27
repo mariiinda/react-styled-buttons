3bb2c15f5804f639975a7ed7890184639c7686da
Marinda <hello@marinda.me>
2019-07-27 22:01:35 +0200

    chore: code tidy

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import { css } from "@emotion/core";
 import { ThemeProvider, withTheme } from "emotion-theming";
 
 import Buttons from "./ui/Buttons";
