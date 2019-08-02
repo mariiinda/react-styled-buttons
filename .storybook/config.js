d8d89121be43d19732509cb1359f0d10483846aa
Marinda <hello@marinda.me>
2019-08-02 18:31:35 +0200

    js tidy

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,4 @@
 import { configure } from "@storybook/react";
-import { withKnobs } from "@storybook/addon-knobs";
-import { withA11y } from "@storybook/addon-a11y";
 
 // automatically import all files ending in *.stories.js
 configure(require.context("../src/", true, /\.stories\.js$/), module);
-
-/* addDecorator(withA11y);
-addDecorator(withKnobs); */
