bbbe187c9484e26c596444618dd09683448dae57
Marinda <hello@marinda.me>
2019-08-02 16:03:37 +0200

    storybook and react-sketch setup

---
+++ sketch/src/my-command.js
@@ -0,0 +1,32 @@
+import * as React from "react";
+import { render, Artboard, Page, Text, View } from "react-sketchapp";
+
+const Document = () => (
+  <Page name="Buttons">
+    <Artboard
+      name="React Styled Buttons"
+      style={{
+        flexDirection: "row",
+        flexWrap: "wrap",
+        width: (96 + 8) * 4
+      }}
+    >
+      <View
+        name={`Demo`}
+        style={{
+          height: 96,
+          width: 96,
+          margin: 4,
+          backgroundColor: "pink",
+          padding: 8
+        }}
+      >
+        <Text>TODO: render theme styleguide here</Text>
+      </View>
+    </Artboard>
+  </Page>
+);
+
+export default () => {
+  render(<Document />, context.document.currentPage());
+};
